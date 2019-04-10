81208869e09f23927739f2b59affdaa9fa4e145b
Regis <25518602+rfjues@users.noreply.github.com>
2019-04-10 09:30:18 -0700

    first crack at .inRange() challenge

--- _.js
+++ _.js
@@ -3,11 +3,27 @@ let _ = {
     let initial = Math.max(num, lower);
     let final = Math.min(initial, upper);
     return final;
+  },
+  
+  inRange(num, start, end){
+    if (end === undefined) {
+      end = start;
+      start = 0;
+    }
+    if (start > end) {
+      start = start + end;
+      end = start - end;
+      start = start - end;
+    }
+    if (num < start) {
+      return false;
+    } else if (num >= end) {
+      return false;
+    } else {
+      return true
+    }
   }
-};
-
-console.log(_.clamp(3, 5, 8))
-
+}
 
 // Do not write or modify code below this line.
 module.exports = _;
