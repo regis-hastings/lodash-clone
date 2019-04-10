952678254787290bbad8ea44dc06bcfb5a2d6522
Regis <25518602+rfjues@users.noreply.github.com>
2019-04-10 08:00:21 -0700

    initial solution for .clam()

--- _.js
+++ _.js
@@ -1,6 +1,15 @@
-let _ = {};
+let _ = {
+  clamp(num, lower, upper){
+    if (num < lower) {
+      num = lower;
+    } else if (num > upper) {
+      num = upper;
+    } 
+    return num;
+  }
+};
 
-
+console.log(_.clamp(10, 5, 8))
 
 
 // Do not write or modify code below this line.
